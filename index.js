0f3dc811ead524578997dcc9ea9bdde366a28ad7
seb jak <sebjak95@gmail.com>
2023-04-24 17:38:52 +0200

    add files

---
+++ index.js
@@ -0,0 +1,36 @@
+let w = innerWidth;
+
+function showModal() {
+  $("#backdrop").removeClass("hide");
+  $("#overlay").removeClass("hide");
+  $("body").addClass("prevent-scroll");
+  $("#closeSVG").removeClass("hide");
+}
+
+function hideModal() {
+  $("#backdrop").addClass("hide");
+  $("#overlay").addClass("hide");
+  $("body").removeClass("prevent-scroll");
+  $("#closeSVG").addClass("hide");
+}
+
+if (w <= 768) {
+  $("#hamburgerSVG").removeClass("hide");
+  $("#navigationDesktop").addClass("hide");
+  $("#hero-img").addClass("hero-img-mobile");
+} else {
+  $("#hero-img").addClass("hero-img-desktop");
+  $("#navigationDesktop").removeClass("hide");
+}
+
+$("#hamburgerSVG").click(() => {
+  showModal();
+});
+
+$("#closeSVG").click(() => {
+  hideModal();
+});
+
+$("#backdrop").click(() => {
+  hideModal();
+});
